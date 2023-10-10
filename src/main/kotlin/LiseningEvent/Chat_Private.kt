d9f564a88adbdbb2be8b66f2e2d3de593650bdc0
escmashu <645937001@qq.com>
2023-10-10 19:49:26 +0800

    feat：新增@bot之后结束对话的方式关闭私有对话的效果

--- src/main/kotlin/LiseningEvent/Chat_Private.kt
+++ src/main/kotlin/LiseningEvent/Chat_Private.kt
@@ -17,29 +17,54 @@ fun chatPrivate (key : String) {
         startsWith(key) quoteReply {
             val getmsg = message.content.substring(key.length)
             val id = message.source.fromId
-
-            //创建给定qq的预设文件,id为文件夹名字可以是qqID可以是groupID
-            InitChatCfg(id,private = true)
-//            if (apikey == "Your apiKey") {
-//                "你没有配置apikey\n请输入/key~你的key"
-//            } else {
-//                if (apikey.matches(tokenPattern)) {
-//                    Openai_http_api(getmsg, ID = ID,role = role)
-//                } else {
-//                    "格式错误"
-//                }
-//            }
-            Get_Chat_Content(getmsg,id)
+            val tempmsg = getmsg.replace(" ","")
+            if (tempmsg == "结束对话"){
+                val path_p: String = path_preset
+                val id_p = message.source.fromId
+                if (File("$path_p/$id_p/message.json").exists()){
+                    File("$path_p/$id_p/message.json").delete()
+                    "已结束"
+                }else{
+                    "你并没有开始对话"
+                }
+            }else{
+                // 创建给定qq的预设文件,id为文件夹名字可以是qqID可以是groupID
+                InitChatCfg(id,private = true)
+            //    if (apikey == "Your apiKey") {
+            //        "你没有配置apikey\n请输入/key~你的key"
+            //    } else {
+            //        if (apikey.matches(tokenPattern)) {
+            //            Openai_http_api(getmsg, ID = ID,role = role)
+            //        } else {
+            //            "格式错误"
+            //        }
+            //    }
+                Get_Chat_Content(getmsg,id)
+            }
         }
 
         "结束对话" reply {
-            val path: String = if (message.source.kind != MessageSourceKind.GROUP) path_preset else "$path_preset/group"
-            val id = if (message.source.kind == MessageSourceKind.GROUP) message.source.targetId else message.source.fromId
-            if (File("$path/$id/message.json").exists()){
-                File("$path/$id/message.json").delete()
-                "已结束"
-            }else{
-                "你并没有开始对话"
+            val path_g: String = "$path_preset/group"
+            val id_g = message.source.targetId
+            val path_p: String = path_preset
+            val id_p = message.source.fromId
+            if(message.source.kind == MessageSourceKind.GROUP)
+            {
+                if (File("$path_g/$id_g/message.json").exists()){
+                    File("$path_g/$id_g/message.json").delete()
+                    "已结束"
+                }else{
+                    "你并没有开始对话"
+                }
+            }
+            else
+            {
+                if (File("$path_p/$id_p/message.json").exists()){
+                    File("$path_p/$id_p/message.json").delete()
+                    "已结束"
+                }else{
+                    "你并没有开始对话"
+                }
             }
         }
     }
